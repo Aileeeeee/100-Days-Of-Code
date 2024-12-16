e655f642a709fead0d3d9396cde8192b6ae7f1fc
Aileeeeee <134634541+Aileeeeee@users.noreply.github.com>
2024-12-16 23:09:48 +0100

    Update Day 1 -  Python Syntax & Data Types.py

--- Day 1 -  Python Syntax & Data Types/Day 1 -  Python Syntax & Data Types.py
+++ Day 1 -  Python Syntax & Data Types/Day 1 -  Python Syntax & Data Types.py
@@ -120,3 +120,59 @@ current_location = input('Enter where you currently live: ').strip().capitalize(
 print('Location Details:')
 print('Place of Birth: ', place_of_birth)
 print('Current Location: ', current_location)
+
+
+
+'''
+Simple Risk Assessment
+Design a program that assesses basic health risks based on user input:
+
+Smokers and those with a BMI ≥ 30 are at "High Risk."
+Others are at "Low Risk."
+'''
+
+# Define a function that collects user input and checks if they are numeric
+def get_user_input(prompt, data_type = int):
+    # Collect user input 
+    user_input = input(prompt)
+
+    # Optimize user input to ensure user input is a positive numeric value
+    while not user_input.replace('.','',).isdigit():
+        print('Enter  a valid positive numeric value')
+        user_input = input(prompt)
+    return data_type(user_input)
+
+# Obtain patient weight from user input
+weight = get_user_input("Enter patient's weight(in kg): ", data_type = float )
+# Check if user input is a positive value
+while weight <= 0:
+    print('Enter a valid positive numeric value')
+    weight = get_user_input("Enter patient's weight(in kg): ", data_type = float )
+
+
+# Obtain patient height from user input
+height = get_user_input("Enter patient's height(in cm): ", data_type = float )
+height = height / 100
+# Check if user input is a positive value
+while height <= 0:
+    print('Enter a valid positive numeric value')
+    height = get_user_input("Enter patient's height(in cm): ", data_type = float )
+    height = height / 100
+
+# Calculate patient bmi
+bmi = weight / (height ** 2)
+
+# Obtain patient's smoking status from user output
+is_smoker = input('Is the patient a smoker? (yes/no): ').strip().capitalize()
+
+# Check patient's health risk level
+if bmi >= 30 or is_smoker.lower() == 'yes':
+    risk_level = 'High risk'
+else:
+    risk_level = 'Low risk'
+    
+# Display output
+print('\nPatient Summary:')
+print("Patient's BMI: ", round(bmi,2))
+print("Patient's Smoking Status: ", is_smoker)
+print("Patient's Health Risk Level: ", risk_level)
